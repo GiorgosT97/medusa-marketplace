83b3636a386b4a405c4f26fc0c3ea1f60361378f
Giorgos Tsoulos <giorgos.tsoulos@fortigo.com>
2026-02-23 21:56:26 +0200

    Fix type cast for StoreAddressModuleService in store-address endpoint

--- marketplace-app/src/api/store/store-address/[storeId]/route.ts
+++ marketplace-app/src/api/store/store-address/[storeId]/route.ts
@@ -1,10 +1,11 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework"
 import { STORE_ADDRESS_MODULE } from "../../../../modules/store-address"
+import StoreAddressModuleService from "../../../../modules/store-address/service"
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
   const { storeId } = req.params
 
-  const storeAddressService = req.scope.resolve(STORE_ADDRESS_MODULE)
+  const storeAddressService = req.scope.resolve<StoreAddressModuleService>(STORE_ADDRESS_MODULE)
 
   const addresses = await storeAddressService.listStoreAddresses({
     store_id: storeId,
